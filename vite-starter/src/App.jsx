66e9916668fc430baea14f4cfcf3213e9f96d028
Webster <weixiao.chen.98@foxmail.com>
2024-06-30 00:09:58 +0800

    handle the button onClick event and add related test

--- vite-starter/src/App.jsx
+++ vite-starter/src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div>
-      <button className="red" >Change to blue</button>
-    </div>
-  );
+    const [buttonColour, setButtonColour] = useState("red");
+    const nextColour = buttonColour === "red" ? "blue" : "red";
+
+    return (
+        <div>
+            <button className={buttonColour} onClick={() => setButtonColour((nextColour))}>Change
+                to {nextColour}</button>
+        </div>
+    );
 }
 
 export default App;
--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -1,10 +1,26 @@
-import { render, screen } from "@testing-library/react";
+import {fireEvent, render, screen} from "@testing-library/react";
 import App from "./App";
 
-test("button starts with correct colour and text", () => {
-  render(<App />);
-  const button = screen.getByRole("button", { name: /blue/i });
-  expect(button).toHaveClass("red");
+describe('button click flow', () => {
+    test("button starts with correct colour and text", () => {
+        render(<App/>);
+
+        const button = screen.getByRole("button", {name: /blue/i});
+
+        expect(button).toHaveClass("red");
+    });
+
+    test("button has correct colour and text after click", () => {
+        render(<App/>);
+
+        const button = screen.getByRole("button", {name: /blue/i});
+        fireEvent.click(button);
+
+        expect(button).toHaveClass("blue");
+        expect(button).toHaveTextContent(/red/i);
+
+        // expect(button).toHaveStyle({"background-color": "blue"});
+    })
 });
 
 
