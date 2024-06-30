68853f4e13270d009bcb95fd0d221595020386da
Webster <weixiao.chen.98@foxmail.com>
2024-06-30 16:36:14 +0800

    refactor button click flow

--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -1,27 +1,17 @@
 import {fireEvent, render, screen} from "@testing-library/react";
 import App from "./App";
+test("button click flow", () => {
+    render(<App/>);
 
-describe('button click flow', () => {
-    test("button starts with correct colour and text", () => {
-        render(<App/>);
+    const button = screen.getByRole("button", {name: /blue/i});
 
-        const button = screen.getByRole("button", {name: /blue/i});
+    expect(button).toHaveClass("red");
 
-        expect(button).toHaveClass("red");
-    });
-
-    test("button has correct colour and text after click", () => {
-        render(<App/>);
-
-        const button = screen.getByRole("button", {name: /blue/i});
-        fireEvent.click(button);
-
-        expect(button).toHaveClass("blue");
-        expect(button).toHaveTextContent(/red/i);
-
-        // expect(button).toHaveStyle({"background-color": "blue"});
-    })
-});
+    fireEvent.click(button);
+    expect(button).toHaveClass("blue");
+    expect(button).toHaveTextContent(/red/i);
+    // expect(button).toHaveStyle({"background-color": "blue"});
+})
 
 test("checkbox flow", () => {
     render(<App />);
