5859988d4a46abebe2f6d3217c9678bdb8f807d5
Webster <weixiao.chen.98@foxmail.com>
2024-06-30 16:56:14 +0800

    disabled button turns gray

--- vite-starter/src/App.jsx
+++ vite-starter/src/App.jsx
@@ -3,12 +3,13 @@ import {useState} from "react";
 
 function App() {
     const [buttonColour, setButtonColour] = useState("red");
-    const nextColour = buttonColour === "red" ? "blue" : "red";
     const [disabled, setDisabled] = useState(false);
+    const nextColour = buttonColour === "red" ? "blue" : "red";
+    const className = disabled ? "gray" : buttonColour;
 
     return (
         <div>
-            <button disabled={disabled} className={buttonColour} onClick={() => setButtonColour((nextColour))}>Change
+            <button disabled={disabled} className={className} onClick={() => setButtonColour((nextColour))}>Change
                 to {nextColour}</button>
 
             <input type="checkbox" id="disable-button-checkbox" checked={disabled} onChange={() => setDisabled((!disabled))} />
--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -1,30 +1,39 @@
 import {fireEvent, render, screen} from "@testing-library/react";
 import App from "./App";
+
 test("button click flow", () => {
     render(<App/>);
 
     const button = screen.getByRole("button", {name: /blue/i});
-
     expect(button).toHaveClass("red");
 
     fireEvent.click(button);
     expect(button).toHaveClass("blue");
     expect(button).toHaveTextContent(/red/i);
     // expect(button).toHaveStyle({"background-color": "blue"});
+
+    const checkbox = screen.getByRole("checkbox", {name: /disable button/i});
+    fireEvent.click(checkbox);
+    expect(button).toHaveClass("gray");
+
+    fireEvent.click(checkbox);
+    expect(button).toHaveClass("blue");
 })
 
 test("checkbox flow", () => {
-    render(<App />);
+    render(<App/>);
 
-    const button = screen.getByRole("button", { name: /blue/i });
-    const checkbox = screen.getByRole("checkbox", { name: /disable button/i });
+    const button = screen.getByRole("button", {name: /blue/i});
+    const checkbox = screen.getByRole("checkbox", {name: /disable button/i});
 
     expect(button).toBeEnabled();
     expect(checkbox).not.toBeChecked();
 
     fireEvent.click(checkbox);
     expect(button).toBeDisabled();
+    expect(button).toHaveClass("gray");
 
     fireEvent.click(checkbox);
     expect(button).toBeEnabled();
+    expect(button).toHaveClass("red");
 })
